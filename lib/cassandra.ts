3695986bb7e01a8bb8b069efb8dcc55b6a795cd8
Dustin Rouillard <me@dustin.sh>
2021-03-14 15:59:06 -0600

    Add no user support for cassandra

--- lib/cassandra.ts
+++ lib/cassandra.ts
@@ -1,24 +1,24 @@
-import { Client, types } from 'cassandra-driver';
+import { Client, DseClientOptions, types } from 'cassandra-driver';
 import { Log } from '@dustinrouillard/fastify-utilities/modules/logger';
 
 const CassandaraConfig = {
   Host: process.env.CASSANDRA_HOST || '127.0.0.1',
   Port: Number(process.env.CASSANDRA_PORT) || 9042,
-  User: process.env.CASSANDRA_USERNAME || 'cassandra',
-  Pass: process.env.CASSANDRA_PASSWORD || 'docker',
+  User: process.env.CASSANDRA_USERNAME as string,
+  Pass: process.env.CASSANDRA_PASSWORD as string,
   Keyspace: process.env.CASSANDRA_KEYSPACE || 'cassandra',
   Datacenter: process.env.CASSANDRA_DATACENTER || 'datacenter1',
 };
 
-export const CassandraClient = new Client({
+const config: DseClientOptions = {
   contactPoints: [`${CassandaraConfig.Host}:${CassandaraConfig.Port}`],
   localDataCenter: CassandaraConfig.Datacenter,
   keyspace: CassandaraConfig.Keyspace,
-  credentials: {
-    username: CassandaraConfig.User,
-    password: CassandaraConfig.Pass,
-  },
-});
+};
+
+if (CassandaraConfig.User && CassandaraConfig.Pass) config.credentials = { username: CassandaraConfig.User, password: CassandaraConfig.Pass };
+
+export const CassandraClient = new Client(config);
 
 (async () => {
   await CassandraClient.connect();
